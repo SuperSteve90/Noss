0ab94fc37385b7923cc4e8d8e18884e2cea2c510
SuperSteve90 <grantgetzlow@outlook.com>
2017-09-28 20:29:08 -0500

    Initial commit

---
+++ Noss/Modules/Test.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace Super_Bot.Modules
+{
+    public class Test : ModuleBase<SocketCommandContext>
+    {
+        [Command("Test")]
+        public async Task test()
+        {
+            await Context.Channel.SendMessageAsync("Testing!");
+        }
+    }
+}
---
+++ Noss/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace Noss
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, "MzYzMTI4NjY3NTYxMjYzMTA1.DK8tmQ.bfA47p-pnEz2mYq2q_iK2RXcEZ0");
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+        }
+    }
+}
